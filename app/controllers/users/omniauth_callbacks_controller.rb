a7f3111db517f0bf18c3cee6976d0de1f1592473
sdaru <sdaru18@gmail.com>
2023-04-18 17:14:05 +0800

    修正錯誤

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,19 +2,16 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.create_from_provider_data(request.env["omniauth.auth"])
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
-  end
-  # 及這段第三方登入失敗的處理方法
-  def failure
-    redirect_to root_path
-  end
 end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-         def self.from_omniauth(access_token)
-          data = access_token.info
-          user = User.where(email: data['email']).first 
-          user
-        end     
+         def self.from_omniauth(auth)
+          find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+            user.email = auth.info.email
+            user.password = Devise.friendly_token[0, 20]
+            #user.name = auth.info.name   # assuming the user model has a name
+            #user.image = auth.info.image # assuming the user model has an image
+            # If you are using confirmable and the provider(s) you use validate emails, 
+            # uncomment the line below to skip the confirmation emails.
+            # user.skip_confirmation!
+          end
+        end 
 end
 
